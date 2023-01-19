b328b265afc11f2b7aec81764abb8aea623750a4
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-01-20 00:53:08 +0300

    angles: understanding angles and trignometry in canvas

--- sketch-02.js
+++ sketch-02.js
@@ -4,6 +4,15 @@ const settings = {
   dimensions: [1080, 1080]
 };
 
+/**
+ * Convert degrees to radians.
+ * @param degrees - The angle in degrees.
+ * @returns the value of the degrees multiplied by the value of pi divided by 180.
+ */
+const degToRad = (degrees) => {
+  return degrees * Math.PI / 180;
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,22 +20,35 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    /* It saves the current state of the canvas. */
-    context.save()
-    context.translate(x, y)
-    context.rotate(0.3)
+    const num = 12;
+    const radius = width * 0.3
 
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    /* It restores the state of the canvas to the state it was in before the last call to
-    `context.save();` */
-    context.restore();
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * i
+
+      x = cx + radius * Math.sin(angle)
+      y = cy + radius * Math.cos(angle)
+
+      /* It saves the current state of the canvas. */
+      context.save()
+
+      context.translate(x, y)
+      context.rotate(-angle)
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+
+      /* It restores the state of the canvas to the state it was in before the last call to
+      `context.save();` */
+      context.restore();
+    }
   };
 };
 
