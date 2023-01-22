1c8e47e30438f3e4e3f154174a294d464700e021
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-01-22 15:26:52 +0300

    Utility functions: added utility functions package

--- sketch-02.js
+++ sketch-02.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [1080, 1080]
@@ -9,9 +11,15 @@ const settings = {
  * @param degrees - The angle in degrees.
  * @returns the value of the degrees multiplied by the value of pi divided by 180.
  */
-const degToRad = (degrees) => {
-  return degrees * Math.PI / 180;
-}
+
+// This has been taken care of by the canvas-sketch-util
+// const degToRad = (degrees) => {
+//   return degrees * Math.PI / 180;
+// }
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -30,7 +38,7 @@ const sketch = () => {
     const radius = width * 0.3
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i
 
       x = cx + radius * Math.sin(angle)
@@ -41,6 +49,7 @@ const sketch = () => {
 
       context.translate(x, y)
       context.rotate(-angle)
+      context.scale(random.range(1, 3), 1)
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
       context.fill();
